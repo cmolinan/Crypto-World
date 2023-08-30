0fd01e3fb5bc4972f8c890540ce0f4fe3efd0418
cmolinan <cmolinan@gmail.com>
2023-08-30 16:56:35 -0500

    Fixes due to change in Coingecko endpoint for chart

--- src/redux/apiCalls.js
+++ src/redux/apiCalls.js
@@ -1,6 +1,11 @@
 const BaseURL = 'https://api.coingecko.com/api/v3/coins/';
 const GetAll = 'markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false';
-const ChartsData = 'market_chart?vs_currency=usd&days=365&interval=weekly';
+
+// const ChartsData = 'market_chart?vs_currency=usd&days=365&interval=weekly';
+// fix due to change in Coingecko endpoint
+const ChartsData = 'market_chart/range?vs_currency=usd&from=';
+const to = Math.floor(Date.now() / 1000); // 'to' date '
+const from = to - 365 * 24 * 3600; // 'from' date
 
 const ApiGetAllCryptos = async () => {
   const response = await fetch(BaseURL + GetAll, {
@@ -14,7 +19,8 @@ const ApiGetAllCryptos = async () => {
 };
 
 export const readChartsData = async (id) => {
-  const url = `${BaseURL}${id}/${ChartsData}`;
+  // fix due to change in Coingecko endpoint
+  const url = `${BaseURL}${id}/${ChartsData}${from}&to=${to}`;
   const response = await fetch(url, {
     method: 'GET',
     headers: {
